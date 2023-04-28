57dd637ac6274bf2c6f9f1b17906f7c0c5d6dd2b
Marco Petri <marco.petri@mail.polimi.it>
2023-04-28 19:45:31 +0200

    Fix doc creation script and workflow

--- create_doc.py
+++ create_doc.py
@@ -9,9 +9,10 @@ if __name__ == "__main__":
         os.system("rm -r ./docs/source/reference/api")
         os.system("rm -r ./docs/source/developer/api")
     
-    os.system("python ./doc_creator.py --max-depth 2 -d -i "
+    os.system("poetry run python ./doc_creator.py --max-depth 2 -d -i "
               "--show-inherited-members --mod-separate anomalearn")
-    os.system("python ./doc_creator.py -a ./developer/api --max-depth 2 -p -P "
-              "-d -i --show-inherited-members --mod-separate anomalearn")
-    os.system("sphinx-build -b html ./docs/source ./docs/stable")
-    os.system("python ./doc_fix_dev_api.py ./docs/stable")
+    os.system("poetry run python ./doc_creator.py -a ./developer/api "
+              "--max-depth 2 -p -P -d -i --show-inherited-members "
+              "--mod-separate anomalearn")
+    os.system("poetry run sphinx-build -b html ./docs/source ./docs/stable")
+    os.system("poetry run python ./doc_fix_dev_api.py ./docs/stable")
